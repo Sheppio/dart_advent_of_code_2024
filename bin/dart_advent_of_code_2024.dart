3ff8d678f245a3b76990ebdbd06b7607f08ecf2a
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2024-12-02 09:15:10 +0000

    updated import

--- bin/dart_advent_of_code_2024.dart
+++ bin/dart_advent_of_code_2024.dart
@@ -1,4 +1,4 @@
-import 'package:dart_advent_of_code_2024/src/day1.dart';
+import '../lib/src/day1.dart';
 
 void main(List<String> arguments) async {
   print('advent_of_code_2024!');
--- test/dart_advent_of_code_2024_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_advent_of_code_2024/dart_advent_of_code_2024.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
