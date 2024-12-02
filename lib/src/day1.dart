7bf51675b391897595e4c4eab70586554f06f160
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2024-12-02 09:28:15 +0000

    day 1 part 2 complete

--- lib/src/day1.dart
+++ lib/src/day1.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 Future<void> day1() async {
   // https://adventofcode.com/2023/day/1/input
   var input = await File('assets/day1_input.txt').readAsString();
-  print(input);
+  //print(input);
   final l1 = <int>[];
   final l2 = <int>[];
   for (var line in input.split('\n')) {
@@ -17,8 +17,21 @@ Future<void> day1() async {
   var totalDiff = 0;
   for (var i = 0; i < l1.length; i++) {
     final diff = (l1[i] - l2[i]).abs();
-    print(diff);
+    //print(diff);
     totalDiff += diff;
   }
-  print(totalDiff);
+  print('Part 1: ${totalDiff.toString()}');
+
+  //Part 2
+  var m = Map.fromIterable(l1.toSet(), key: (k) => k, value: (v) => 0);
+  for (var element in l2) {
+    if (m.containsKey(element)) {
+      m[element] = m[element]! + 1;
+    }
+  }
+  var similarityScore = 0;
+  for (var value in l1) {
+    similarityScore += value * m[value]!;
+  }
+  print('Part 2: ${similarityScore.toString()}');
 }
