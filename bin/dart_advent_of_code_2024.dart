9b9eda3b6efe04e4e0f55d05cc0a53aac5ce6c86
Sheppio <mark_sheppard_1977@hotmail.com>
2024-12-02 07:33:04 +0000

    first commit

---
+++ bin/dart_advent_of_code_2024.dart
@@ -0,0 +1,5 @@
+import 'package:dart_advent_of_code_2024/dart_advent_of_code_2024.dart' as dart_advent_of_code_2024;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_advent_of_code_2024.calculate()}!');
+}
---
+++ test/dart_advent_of_code_2024_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_advent_of_code_2024/dart_advent_of_code_2024.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
